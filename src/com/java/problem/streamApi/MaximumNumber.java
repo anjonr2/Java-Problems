242d22bf942a2229e614baeba75df5ae2c066874
Anjon Roy <anjon782446@gmail.com>
2026-08-24 15:44:18 +0530

    Max number

--- src/com/java/problem/streamApi/MaximumNumber.java
+++ src/com/java/problem/streamApi/MaximumNumber.java
@@ -3,13 +3,14 @@ package com.java.problem.streamApi;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class MaximumNumber {
     public static void main(String [] args){
         List<Integer> numbers = Arrays.asList(5, 10, 2, 8, 20);
 
-        Integer max = numbers.stream()
-                .reduce(0, (a,b)-> Math.max(a,b));
+        Optional<Integer> max = numbers.stream()
+                .reduce((a,b)-> Math.max(a,b));
         System.out.println(max);
     }
 }
